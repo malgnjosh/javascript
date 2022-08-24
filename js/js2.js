6572aeb875fe460f845bd775a486f0e34b3e46bf
malgnjosh <josh@malgnsoft.com>
2022-08-24 23:41:01 +0900

    hi

--- js/js2.js
+++ js/js2.js
@@ -22,6 +22,55 @@ document.getElementById('btn5').onclick = function() {
     document.getElementById('output5').innerHTML = typeof(+ "10");
 }
 
+document.getElementById('btn6').onclick = function() {
+    document.getElementById('output6').innerHTML = parseInt("abc");
+}
 
+document.getElementById('btn6').onclick = function() {
+    document.getElementById('output6').innerHTML = parseInt("abc");
+}
 
+document.getElementById('btn7').onclick = function() {
+    document.getElementById('output7').innerHTML = parseInt("abc") + 1;
+}
 
+document.getElementById('btn8').onclick = function() {
+    var result8 = parseInt("abc");
+    if(Number.isNaN(result8)) result8 = "숫자가 아님";
+    //if(NaN == result) result = "숫자가 아님";
+    document.getElementById('output8').innerHTML = result8;
+}
+
+document.getElementById('btn9').onclick = function() {
+    document.getElementById('output9').innerHTML = 1/0;
+}
+
+document.getElementById('btn10').onclick = function() {
+    document.getElementById('output10').innerHTML = isFinite(1/0);
+}
+
+var arr1 = ["abc", "def"];
+document.getElementById('btn11').onclick = function() {
+    var arr2 = ["ghi"];
+    var arr3 = ["jkl"];
+    arr1 = arr1.concat(arr2, arr3)
+    document.getElementById('output11').innerHTML = arr1;
+}
+
+document.getElementById('btn12').onclick = function() {
+    arr1.fill("ABC");
+    document.getElementById('output12').innerHTML = arr1;
+}
+
+document.getElementById('btn13').onclick = function() {
+    arr1.fill("lll", 2, 4);
+    document.getElementById('output13').innerHTML = arr1;
+}
+
+document.getElementById('btn14').onclick = function() {
+    // const result14 = arr1.filter(function(str){
+    //     return str === "lll";
+    // });
+    const result14 = arr1.filter((str) => str === "lll");
+    document.getElementById('output14').innerHTML = result14;
+}
