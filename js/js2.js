9539f8f66511d5f26607b34dc7ecdaa130664000
josh <josh@malgnsoft.com>
2022-09-29 18:52:23 +0900

    a

--- js/js2.js
+++ js/js2.js
@@ -1,4 +1,4 @@
-result1 = 0.1 + 0.2;
+result1 = (- "0.3") + 0.1;
 document.getElementById('btn1').onclick = function() {
     document.getElementById('output1').innerHTML = result1;
 }
@@ -62,8 +62,6 @@ document.getElementById('btn11.2').onclick = function() {
     document.getElementById('output11.2').innerHTML = arr1;
 }
 
-
-
 document.getElementById('btn12').onclick = function() {
     arr1.fill("ABC");
     document.getElementById('output12').innerHTML = arr1;
@@ -78,7 +76,7 @@ document.getElementById('btn14').onclick = function() {
     // const result14 = arr1.filter(function(str){
     //     return str === "lll";
     // });
-    const result14 = arr1.filter((str) => str === "lll");
+    const result14 = arr1.filter((str) => false);
     document.getElementById('output14').innerHTML = result14;
 }
 
@@ -118,12 +116,26 @@ document.getElementById('btn17_3').onclick = function() {
 }
 
 document.getElementById('btn18').onclick = function() {
-    document.getElementById('output18').innerHTML = dogs.indexOf("mydog");
+    document.getElementById('output18').innerHTML = dogs.indexOf("notmydog");
 }
 
 document.getElementById('btn19').onclick = function() {
     dogs.forEach(function(currentDog, index, array){
         if(currentDog === "mydog")
-            document.getElementById('output19').innerHTML = index + " " + array;
+            document.getElementById('output19').innerHTML = index + " " + this;
     })
 }
+//jquery each
+
+//false
+if(!false) console.log("false");
+if(!null) console.log("null");
+if(!0) console.log("0");
+if(!NaN) console.log("NaN");
+if(!undefined) console.log("undefined");
+if(!"") console.log("empty");
+
+//true
+if(true) console.log("true");
+if(1) console.log("not zero");
+if("abc") console.log("not empty");
